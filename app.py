7a4eb9e757897bc4451110bb00c9331cde6b048e
Ubuntu <ubuntu@ip-172-31-26-62.ec2.internal>
2026-02-13 15:07:46 +0000

    New commit

--- app.py
+++ app.py
@@ -3,12 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-<<<<<<< HEAD
     return "Pipeline is working LIVE! ✅"
 
 # Optional: for local run (not used in prod)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
-=======
-    return "Hello Malik enjoy.....: ! CI/CD working!"
->>>>>>> 52dc545 (trigger pipeline)
